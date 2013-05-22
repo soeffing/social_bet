4bfbd3d97f786c58df3249331cb1b0496c87ea16
soeffing <ulrich_soeffing@gmx.de>
2013-05-22 17:32:23 +0100

    done with server send events

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,9 +4,11 @@ SocialBet::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  # IMPORTANT: usually false but set to true for server send event
   config.cache_classes = true
 
   # rails 4 stuff
+  # IMPORTANT: usually false but set to true for server send event
   config.eager_load = true
   # Log error messages when you accidentally call methods on nil.
   # deprecated rails 4
